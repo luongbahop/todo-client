c3e55a53b471668985401e97871195b2482a2e4e
Nguyen Dinh Vu <vund.vn@gmail.com>
2017-11-28 10:17:17 +0700

    setup project

---
+++ src/components/Layout/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+export const MenuWrap = styled.div`
+  font-size: 2em;
+  text-align:center;
+  position:fixed;
+  display:block;
+  top:0;
+  left:0;
+  right:0;
+  padding: 8px 0;
+  border-bottom: 1px solid #eeeeee;
+  background:#fff;
+  z-index:999
+`;
+export const MenuWrapContainer = styled.div`
+  max-width:1127px;
+  margin: auto;
+`;
+
+export const MenuUl = styled.ul`
+list-style:none;
+float:left;
+`;
+
+export const MenuLi = styled.li`
+float:left
+`;
---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Header from './Header';
+import Footer from './Footer';
+import Layout from './Layout';
+import Authentication from './Authentication';
+export{
+  Header,
+  Footer,
+  Layout,
+  Authentication
+}
